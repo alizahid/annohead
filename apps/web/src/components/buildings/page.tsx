a0bcc9126894f6c759d6ea7efdc904655da3a868
Ali Zahid <ali.zahid@live.com>
2026-09-20 12:11:52 +0500

    Styles updated

--- apps/web/src/components/buildings/page.tsx
+++ apps/web/src/components/buildings/page.tsx
@@ -48,182 +48,193 @@ export function BuildingPage({ building }: Props) {
       </div>
 
       <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {building.costs.length ? (
-          <DataList.Root title={t('construction')}>
-            {building.costs.map((item) => (
-              <DataList.Link
-                icon={item.icon}
-                id={item.guid}
-                key={item.guid}
-                name={item.name}
-                type="product"
-                value={item.amount}
-              />
-            ))}
-          </DataList.Root>
-        ) : null}
+        <div className="flex flex-col gap-6 empty:hidden">
+          {building.costs.length ? (
+            <DataList.Root title={t('construction')}>
+              {building.costs.map((item) => (
+                <DataList.Link
+                  icon={item.icon}
+                  id={item.guid}
+                  key={item.guid}
+                  name={item.name}
+                  type="product"
+                  value={item.amount}
+                />
+              ))}
+            </DataList.Root>
+          ) : null}
 
-        {building.maintenance.length ? (
-          <DataList.Root title={t('maintenance')}>
-            {building.maintenance.map((item) => (
+          {building.maintenance.length ? (
+            <DataList.Root title={t('maintenance')}>
+              {building.maintenance.map((item) => (
+                <DataList.Item
+                  icon={item.icon}
+                  key={item.guid}
+                  name={item.name}
+                  value={item.amount}
+                />
+              ))}
+            </DataList.Root>
+          ) : null}
+        </div>
+
+        <div className="flex flex-col gap-6 empty:hidden">
+          {building.inputs.length ? (
+            <DataList.Root title={t('inputs')}>
+              {building.inputs.map((item) => (
+                <DataList.Link
+                  icon={item.icon}
+                  id={item.guid}
+                  key={item.guid}
+                  name={item.name}
+                  type="product"
+                  value={item.amount}
+                />
+              ))}
+            </DataList.Root>
+          ) : null}
+
+          {building.outputs.length ? (
+            <DataList.Root title={t('outputs')}>
+              {building.outputs.map((item) => (
+                <DataList.Link
+                  icon={item.icon}
+                  id={item.guid}
+                  key={item.guid}
+                  name={item.name}
+                  type="product"
+                  value={item.amount}
+                />
+              ))}
+            </DataList.Root>
+          ) : null}
+
+          {building.workforce.length ? (
+            <DataList.Root title={t('workforce')}>
+              {building.workforce.map((item) => (
+                <DataList.Item
+                  icon={item.icon}
+                  key={item.guid}
+                  name={item.name}
+                  value={item.amount}
+                />
+              ))}
+            </DataList.Root>
+          ) : null}
+        </div>
+
+        <div className="flex flex-col gap-6 empty:hidden">
+          {building.effects.length ? (
+            <DataList.Root title={t('effects')}>
+              {building.effects.map((item) => (
+                <DataList.Item
+                  icon={item.attribute ? getIcon(item.attribute) : null}
+                  key={`${item.buildingGuid}:${item.attribute}`}
+                  name={item.name}
+                  value={
+                    item.value
+                      ? f.number(
+                          item.isPercent ? item.value / 100 : item.value,
+                          {
+                            signDisplay: 'always',
+                            style: item.isPercent ? 'percent' : undefined,
+                          },
+                        )
+                      : undefined
+                  }
+                />
+              ))}
+            </DataList.Root>
+          ) : null}
+
+          {building.buffs.length ? (
+            <DataList.Root title={t('buffs')}>
+              {building.buffs.map((item) => (
+                <DataList.Item
+                  icon={item.attribute ? getIcon(item.attribute) : undefined}
+                  key={`${item.buildingGuid}:${item.attribute}`}
+                  name={item.name}
+                  value={
+                    item.value
+                      ? f.number(item.value, {
+                          signDisplay: 'always',
+                        })
+                      : undefined
+                  }
+                />
+              ))}
+            </DataList.Root>
+          ) : null}
+        </div>
+
+        <div className="flex flex-col gap-6 empty:hidden">
+          {building.unlockedBy.length ? (
+            <DataList.Root title={t('unlocked')}>
+              {building.unlockedBy.map((item) => (
+                <DataList.Link
+                  icon={item.icon}
+                  id={item.guid}
+                  key={item.guid}
+                  name={item.name}
+                  type="tech"
+                />
+              ))}
+            </DataList.Root>
+          ) : null}
+
+          <DataList.Root title={t('other.title')}>
+            {building.baseProductivity ? (
               <DataList.Item
-                icon={item.icon}
-                key={item.guid}
-                name={item.name}
-                value={item.amount}
+                name={t('other.baseProductivity')}
+                value={f.number(building.baseProductivity / 100, {
+                  style: 'percent',
+                })}
               />
-            ))}
-          </DataList.Root>
-        ) : null}
+            ) : null}
 
-        {building.inputs.length ? (
-          <DataList.Root title={t('inputs')}>
-            {building.inputs.map((item) => (
-              <DataList.Link
-                icon={item.icon}
-                id={item.guid}
-                key={item.guid}
-                name={item.name}
-                type="product"
-                value={item.amount}
-              />
-            ))}
-          </DataList.Root>
-        ) : null}
-
-        {building.outputs.length ? (
-          <DataList.Root title={t('outputs')}>
-            {building.outputs.map((item) => (
-              <DataList.Link
-                icon={item.icon}
-                id={item.guid}
-                key={item.guid}
-                name={item.name}
-                type="product"
-                value={item.amount}
-              />
-            ))}
-          </DataList.Root>
-        ) : null}
-
-        {building.workforce.length ? (
-          <DataList.Root title={t('workforce')}>
-            {building.workforce.map((item) => (
+            {building.cycleTime ? (
               <DataList.Item
-                icon={item.icon}
-                key={item.guid}
-                name={item.name}
-                value={item.amount}
+                name={t('other.cycleTime')}
+                value={f.number(building.cycleTime, {
+                  style: 'unit',
+                  unit: 'second',
+                })}
               />
-            ))}
-          </DataList.Root>
-        ) : null}
+            ) : null}
 
-        {building.effects.length ? (
-          <DataList.Root title={t('effects')}>
-            {building.effects.map((item) => (
+            {building.radius ? (
+              <DataList.Item name={t('other.radius')} value={building.radius} />
+            ) : null}
+
+            {building.transporterRange ? (
               <DataList.Item
-                icon={item.attribute ? getIcon(item.attribute) : null}
-                key={`${item.buildingGuid}:${item.attribute}`}
-                name={item.name}
-                value={
-                  item.value
-                    ? f.number(item.isPercent ? item.value / 100 : item.value, {
-                        signDisplay: 'always',
-                        style: item.isPercent ? 'percent' : undefined,
-                      })
-                    : undefined
-                }
+                name={t('other.transporterRange')}
+                value={building.transporterRange}
               />
-            ))}
-          </DataList.Root>
-        ) : null}
+            ) : null}
 
-        {building.buffs.length ? (
-          <DataList.Root title={t('buffs')}>
-            {building.buffs.map((item) => (
+            {building.streetRadius ? (
               <DataList.Item
-                icon={item.attribute ? getIcon(item.attribute) : undefined}
-                key={`${item.buildingGuid}:${item.attribute}`}
-                name={item.name}
-                value={
-                  item.value
-                    ? f.number(item.value, {
-                        signDisplay: 'always',
-                      })
-                    : undefined
-                }
+                name={t('other.streetRadius')}
+                value={building.streetRadius}
               />
-            ))}
-          </DataList.Root>
-        ) : null}
+            ) : null}
 
-        {building.unlockedBy.length ? (
-          <DataList.Root title={t('unlocked')}>
-            {building.unlockedBy.map((item) => (
-              <DataList.Link
-                icon={item.icon}
-                id={item.guid}
-                key={item.guid}
-                name={item.name}
-                type="tech"
+            {building.category ? (
+              <DataList.Item
+                name={t('other.category')}
+                value={building.category}
               />
-            ))}
+            ) : null}
+
+            {building.template ? (
+              <DataList.Item
+                name={t('other.template')}
+                value={building.template}
+              />
+            ) : null}
           </DataList.Root>
-        ) : null}
-
-        <DataList.Root title={t('other.title')}>
-          {building.baseProductivity ? (
-            <DataList.Item
-              name={t('other.baseProductivity')}
-              value={f.number(building.baseProductivity / 100, {
-                style: 'percent',
-              })}
-            />
-          ) : null}
-
-          {building.cycleTime ? (
-            <DataList.Item
-              name={t('other.cycleTime')}
-              value={f.number(building.cycleTime, {
-                style: 'unit',
-                unit: 'second',
-              })}
-            />
-          ) : null}
-
-          {building.radius ? (
-            <DataList.Item name={t('other.radius')} value={building.radius} />
-          ) : null}
-
-          {building.transporterRange ? (
-            <DataList.Item
-              name={t('other.transporterRange')}
-              value={building.transporterRange}
-            />
-          ) : null}
-
-          {building.streetRadius ? (
-            <DataList.Item
-              name={t('other.streetRadius')}
-              value={building.streetRadius}
-            />
-          ) : null}
-
-          {building.category ? (
-            <DataList.Item
-              name={t('other.category')}
-              value={building.category}
-            />
-          ) : null}
-
-          {building.template ? (
-            <DataList.Item
-              name={t('other.template')}
-              value={building.template}
-            />
-          ) : null}
-        </DataList.Root>
+        </div>
       </div>
 
       <CommentList guid={building.guid} />
--- apps/web/src/components/data-list/item.tsx
+++ apps/web/src/components/data-list/item.tsx
@@ -12,7 +12,7 @@ export function Item({ name, icon, value }: Props) {
   const f = useFormatter()
 
   return (
-    <div className="flex items-center justify-between gap-4 rounded-sm outline-none ring-accent-8 ring-offset-4 ring-offset-gray-2 focus-visible:ring-2">
+    <div className="flex items-center justify-between gap-4 rounded-sm outline-none ring-accent-8 ring-offset-4 ring-offset-gray-2 empty:hidden focus-visible:ring-2">
       <div className="flex flex-1 items-center gap-2">
         {icon ? <Icon className="size-6" icon={icon} /> : null}
 
