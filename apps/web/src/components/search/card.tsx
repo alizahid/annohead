5b5d4c85edb267ce3946aa4e0e8764fe2eb2f93e
Ali Zahid <ali.zahid@live.com>
2026-09-26 17:58:56 +0500

    Search card updated

--- apps/web/src/components/search/card.tsx
+++ apps/web/src/components/search/card.tsx
@@ -1,10 +1,12 @@
 import { type SearchHit } from '@anno/db/client'
+import { useTranslations } from 'next-intl'
 
 import { NavLink } from '@/intl/nav'
 import { getIcon } from '@/lib/icons'
 import { getUrl } from '@/lib/url'
 
 import { Icon } from '../common/icon'
+import { Tooltip } from '../common/tooltip'
 import { ItemIcon } from '../items/icon'
 import { RegionCard } from '../shared/region'
 
@@ -13,6 +15,8 @@ type Props = {
 }
 
 export function SearchCard({ item }: Props) {
+  const t = useTranslations('component.search.card')
+
   return (
     <NavLink
       className="flex gap-4 rounded-lg p-4 outline-none ring-accent-8 transition-colors hover:bg-accent-4 focus-visible:ring-2"
@@ -26,17 +30,15 @@ export function SearchCard({ item }: Props) {
         ) : (
           <Icon className="size-16" icon={getIcon(`ui.${item.type}`)} />
         )}
-
-        {item.icon ? (
-          <div className="absolute right-0 bottom-0 flex rounded-full bg-accent-2 p-1">
-            <Icon className="size-4" icon={getIcon(`ui.${item.type}`)} />
-          </div>
-        ) : null}
       </aside>
 
       <div className="flex min-w-0 flex-1 flex-col gap-1">
         <div className="flex gap-2">
           <div className="text-pretty font-bold">{item.name}</div>
+
+          <Tooltip content={t(`type.${item.type}`)} render={<div />}>
+            <Icon className="size-6" icon={getIcon(`ui.${item.type}`)} />
+          </Tooltip>
 
           {item.type === 'building' ||
           item.type === 'chain' ||
