167be564c5ec02d1c1394ed255839fc31b21451e
Ali Zahid <ali.zahid@live.com>
2026-09-26 18:21:36 +0500

    Styles updated

--- apps/web/src/components/common/tooltip.tsx
+++ apps/web/src/components/common/tooltip.tsx
@@ -15,7 +15,7 @@ export function Tooltip({ children, content, offset = 4, render }: Props) {
 
       <Component.Portal>
         <Component.Positioner sideOffset={offset}>
-          <Component.Popup className="relative rounded-md bg-black px-1.5 py-0.5 font-bold text-sm text-white dark:bg-white dark:text-black">
+          <Component.Popup className="relative rounded-sm bg-black px-1.5 py-0.5 font-bold text-sm text-white dark:bg-white dark:text-black">
             <Component.Arrow className="before:transform-[translate(-50%,50%)_rotate(45deg)] relative block h-1.5 w-3 overflow-clip before:absolute before:bottom-0 before:left-1/2 before:h-[calc(6px*sqrt(2))] before:w-[calc(6px*sqrt(2))] before:bg-black before:content-[''] data-[side=bottom]:-top-1.5 data-[side=left]:-right-2.25 data-[side=top]:-bottom-1.5 data-[side=right]:-left-2.25 data-[side=left]:rotate-90 data-[side=right]:-rotate-90 data-[side=top]:rotate-180 dark:before:bg-white" />
 
             {content}
